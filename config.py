e72f529abfec646f7f609d552f05fe88f5ce74cd
God Luffy <131106303+Chandansinghbot@users.noreply.github.com>
2024-11-16 14:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
- cloud_name="duaev5c6u",
- api_key="812334987785874",
- api_secret="yp5NLb2921qVKEvmyzvwViQE_54",
+ cloud_name="duaev5c6u"
+ api_key="812334987785874"
+ api_secret="yp5NLb2921qVKEvmyzvwViQE_54"
  secure=True
